50184ad876b3f2f05045e1dad54ae1068fd32ba8
ChandraAwesome <120436175+ChandraAwesome@users.noreply.github.com>
2023-01-24 16:39:06 +0530

    [Chandra]-RangeofPrime

---
+++ RangeofPrimeNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_Algorithms
+{
+    public class RangeofPrimeNumbers
+    {
+        public static void Prime()
+        {
+                int n = 1000;
+                for (int i = 2; i <= n; i++)
+                {
+                    bool isPrime = true;
+                    for (int j = 2; j <= Math.Sqrt(i); j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                    if (isPrime)
+                        Console.Write(i + " ");
+                }
+            
+        }
+
+    }
+}
